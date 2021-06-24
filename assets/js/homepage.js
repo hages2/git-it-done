c9723801de640ad52df95409d02a2e8d82633565
Stephanie Hage <stephaniehage23@gmail.com>
2021-06-24 13:44:04 -0400

    redirect

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,34 +3,6 @@ var repoSearchTerm = document.querySelector("#repo-search-term");
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 
-
-
-var getUserRepos = function (user) {
-    //format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-    //make a request to the url
-    fetch(apiUrl)
-        .then(function (response) {
-            //request was successful
-            if (response.ok) {
-                response.json().then(function (data) {
-                    displayRepos(data, user);
-                });
-            } else {
-                alert("Error: GitHub User Not Found");
-            }
-        })
-        .catch(function (error) {
-            //Notice this .catch() getting chained onto the end of the .then() method
-            alert("Unable to connect to GitHub");
-        });
-}
-
-
-var response = fetch("https://api.github.com/users/octocat/repos")
-console.log(response);
-
 var formSubmitHandler = function (event) {
     event.preventDefault();
     // get value from input element
@@ -47,6 +19,29 @@ var formSubmitHandler = function (event) {
     console.log(event);
 };
 
+var getUserRepos = function (user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl)
+        .then(function (response) {
+            //request was successful
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            //Notice this .catch() getting chained onto the end of the .then() method
+            alert("Unable to connect to GitHub");
+        });
+}
+
 var displayRepos = function (repos, searchTerm) {
     //check if api returned any repos
     if (repos.length === 0) {
@@ -54,14 +49,18 @@ var displayRepos = function (repos, searchTerm) {
         return;
     }
 
+    repoSearchTerm.textContent = searchTerm;
+    
     //loop over repos
     console.log(repos)
     for (var i = 0; i < repos.length; i++) {
         //format repo name
-        var repoName = repos[i].owner.login + "/" + repos[i].name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
         //create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
 
         //create a span element to hold repository name 
         var titleEl = document.createElement("span");
